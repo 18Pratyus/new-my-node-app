9ccd2eb9c55cefd406b1dac9fd160f846060dda5
pratyus kumar mansingh <pratyusmansingh@pratyuss-MacBook-Air.local>
2026-07-18 01:11:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end("<h1>Hello ! This is Pratyus from Node.js 🚀</h1>");
+
+});
+
+
+
+server.listen(3000, () => {
+
+    console.log("Server running on port 3000");
+
+});
